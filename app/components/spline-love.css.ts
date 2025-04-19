74d285123f1866a0510dbb2bcfb34aa6beb41b5e
sne12345 <sne12345@naver.com>
2025-04-19 17:30:11 +0900

    fix main image ratio

--- app/components/spline-love.css.ts
+++ app/components/spline-love.css.ts
@@ -10,9 +10,26 @@ import { style } from "@vanilla-extract/css";
 export const container = style({
   width: "100%",
   aspectRatio: "658 / 333", // 비율 고정
-  background: "RGB(241, 206, 186)",
+  // background: "RGB(241, 206, 186)",
   position: "relative",
   // overflow: "hidden",       // 필요 시
+});
+
+
+// ✅ 반응형 이미지 스타일
+export const image = style({
+  width: "80%",
+  height: "100%",
+  objectFit: "fill",
+  display: "block",
+  margin: "0 auto", // 가운데 정렬
+
+  "@media": {
+    "screen and (min-width: 600)": {
+      width: "60%", // PC일 때 이미지 너비 줄임
+      height: "auto", // 자동으로 맞춤
+    },
+  },
 });
 
 export const loading = style({
--- app/components/spline-love.tsx
+++ app/components/spline-love.tsx
@@ -1,25 +1,26 @@
 
 "use client";
 
-import { container, icon,loading } from "./spline-love.css";
+import { container, icon, image, loading } from "./spline-love.css";
 import { LeavesArt } from "./svgs";
 
 export default function SplineLove() {
   return (
 
     <div className={container}>
-      <div className={icon}>
+      {/* <div className={icon}>
         <LeavesArt />
-      </div>
+      </div> */}
       <img
         src="/gallery/main-photo10.png" // 이미지 경로
         alt="메인 이미지"
-        style={{
-          width: "100%",
-          height: "100%",            // 세로 비율 유지
-          objectFit: "cover",      // 넘치지 않게
-          display: "block", // ✅ 줄 간격 제거
-        }}
+        className={image}
+        // style={{
+        //   width: "100%",
+        //   height: "100%",            // 세로 비율 유지
+        //   objectFit: "cover",      // 넘치지 않게
+        //   display: "block", // ✅ 줄 간격 제거
+        // }}
       />
     </div>
   );
