6155849340890f0a564ca3630f5953e68c0e6b8e
sne12345 <sne12345@naver.com>
2025-05-14 00:45:21 +0900

    Updates

---
+++ _next/static/ZgGC-7njcG_Y2PQuQsBpr/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-fcbe99b634a545bf.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
